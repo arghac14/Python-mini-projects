f21baa730beb808b22e6f87f0d2d4a6eb10cae98
Arghadip Chakraborty <chakrabortyarghadip5@gmail.com>
2019-01-24 20:20:19 +0530

    Word puzzle game which is played by scrambling the letters to make an anagram.

---
+++ Jumble word game/Jumble Words.py
@@ -0,0 +1,17 @@
+import time
+
+print("Say Something.",end='')
+p=0
+while(p < 1):
+    time.sleep(0.6)
+    print(".",end='')
+    time.sleep(0.5) 
+    print(".",end='')
+    time.sleep(0.37)
+    print(".",end='')
+    time.sleep(0.30)
+    print(".",end='')
+    p=p+1
+r=sr.Recgonizer()
+
+        
