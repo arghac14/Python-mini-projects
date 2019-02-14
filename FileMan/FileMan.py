af811f39a882d859aaffa629493ec2167b7773fe
Arghadip Chakraborty <chakrabortyarghadip5@gmail.com>
2019-02-14 21:47:08 +0530

    A python script to create, manipulate, organize, search, count the files and directories at a user provided location.

--- FileMan/FileMan.py
+++ FileMan/FileMan.py
@@ -63,35 +63,6 @@ def searchFile(BASE_DIR,ss):
 
 
 
-DIRECTORIES = {
-    "HTML": [".html5", ".html", ".htm", ".xhtml"],
-    "IMAGES": [".jpeg", ".jpg", ".tiff", ".gif", ".bmp", ".png", ".bpg", "svg",
-               ".heif", ".psd"],
-    "VIDEOS": [".avi", ".flv", ".wmv", ".mov", ".mp4", ".webm", ".vob", ".mng",
-               ".qt", ".mpg", ".mpeg", ".3gp", ".mkv"],
-    "DOCUMENTS": [".oxps", ".epub", ".pages", ".docx", ".doc", ".fdf", ".ods",
-                  ".odt", ".pwi", ".xsn", ".xps", ".dotx", ".docm", ".dox",
-                  ".rvg", ".rtf", ".rtfd", ".wpd", ".xls", ".xlsx", ".ppt",
-                  "pptx"],
-    "ARCHIVES": [".a", ".ar", ".cpio", ".iso", ".tar", ".gz", ".rz", ".7z",
-                 ".dmg", ".rar", ".xar", ".zip"],
-    "AUDIO": [".aac", ".aa", ".aac", ".dvf", ".m4a", ".m4b", ".m4p", ".mp3",
-              ".msv", "ogg", "oga", ".raw", ".vox", ".wav", ".wma"],
-    "PLAINTEXT": [".txt", ".in", ".out"],
-    "PDF": [".pdf"],
-    "PYTHON": [".py"],
-    "XML": [".xml"],
-    "EXE": [".exe"],
-    "SHELL": [".sh"]
-
-}
-
-
-FILE_FORMATS = {file_format: directory
-                for directory, file_formats in DIRECTORIES.items()
-                for file_format in file_formats}
-
-
 def organize():
     DIRECTORIES = {
     "HTML": [".html5", ".html", ".htm", ".xhtml"],
